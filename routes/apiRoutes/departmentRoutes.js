d1f40173e50070ad4af7602d726ab18df546ad14
Adam Field <adamjfield@gmail.com>
2021-12-11 10:20:50 -0600

    add input test and route to add department

--- routes/apiRoutes/departmentRoutes.js
+++ routes/apiRoutes/departmentRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
 
 // get all departments
 router.get('/departments', (req, res) => {
@@ -18,7 +19,7 @@ router.get('/departments', (req, res) => {
 });
 
 // get department by id
-router.get('/departments/:id', (req, res) => {
+router.get('/department/:id', (req, res) => {
   const sql = `SELECT * FROM departments WHERE id = ?`;
   const params = [req.params.id];
   db.query(sql, params, (err, row) => {
@@ -33,9 +34,30 @@ router.get('/departments/:id', (req, res) => {
   });
 });
 
+// add a new department
+router.post('/department', ({ body }, res) => {
+  const errors = inputCheck(body, 'dept_name');
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `INSERT INTO departments (dept_name) VALUES (?)`;
+  const params = [body.dept_name];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body,
+    });
+  });
+});
+
 // delete department by id
-router.get('/departments/:id', (req, res) => {
-  const sql = `DROP FROM departments WHERE id = ?`;
+router.delete('/department/:id', (req, res) => {
+  const sql = `DELETE FROM departments WHERE id = ?`;
   const params = [req.params.id];
   db.query(sql, params, (err, result) => {
     if (err) {
